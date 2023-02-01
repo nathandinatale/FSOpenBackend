98f64c7fe77a0cb32b8c269212a2e118cde65690
nathandinatale <ndinatal@uwo.ca>
2023-02-01 14:16:39 +0200

    Fixed error handler

--- index.js
+++ index.js
@@ -101,14 +101,15 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
-const errorHandler = (error, request, response) => {
+const errorHandler = (error, request, response, next) => {
   // console.log(error);
   if (error.name === "CastError")
     return response.status(400).send({ error: "malformatted id" }).end();
 
-  if (error.name === "ValidationError")
-    return response.status(400).json({ error: error.message });
-  response.status(500).end();
+  if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message }).end();
+  }
+  next(error);
 };
 
 app.use(unknownEndpoint);
